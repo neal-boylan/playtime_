e8c2d90c5fbb48b8c4f9756b110d5a3cbb9ba890
neal-boylan <nealboylan@gmail.com>
2024-03-21 16:24:44 +0000

    Added playlist view and ability to add/list tracks

--- src/web-routes.js
+++ src/web-routes.js
@@ -1,5 +1,6 @@
 import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
+import { playlistController } from "./controllers/playlist-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 
 export const webRoutes = [
@@ -12,6 +13,9 @@ export const webRoutes = [
 
   { method: "GET", path: "/dashboard", config: dashboardController.index },
   { method: "POST", path: "/dashboard/addplaylist", config: dashboardController.addPlaylist },
+
+	{ method: "GET", path: "/playlist/{id}", config: playlistController.index },
+  { method: "POST", path: "/playlist/{id}/addtrack", config: playlistController.addTrack },
 
 	{ method: "GET", path: "/about", config: aboutController.index },
 ];
