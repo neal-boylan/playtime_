5a12bdbf3f3c7472248269cc558221acd5715f30
neal-boylan <nealboylan@gmail.com>
2024-03-22 12:58:50 +0000

    Added function to delete playlist/tracks, env file, cookies

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -3,9 +3,11 @@ import { db } from "../models/db.js";
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
-      const playlists = await db.playlistStore.getAllPlaylists();
+      const loggedInUser = request.auth.credentials;
+      const playlists = await db.playlistStore.getUserPlaylists(loggedInUser._id);
       const viewData = {
         title: "Playtime Dashboard",
+        user: loggedInUser,
         playlists: playlists,
       };
       return h.view("dashboard-view", viewData);
@@ -14,10 +16,20 @@ export const dashboardController = {
 
   addPlaylist: {
     handler: async function (request, h) {
+      const loggedInUser = request.auth.credentials;
       const newPlayList = {
+        userid: loggedInUser._id,
         title: request.payload.title,
       };
       await db.playlistStore.addPlaylist(newPlayList);
+      return h.redirect("/dashboard");
+    },
+  },
+
+  deletePlaylist: {
+    handler: async function (request, h) {
+      const playlist = await db.playlistStore.getPlaylistById(request.params.id);
+      await db.playlistStore.deletePlaylistById(playlist._id);
       return h.redirect("/dashboard");
     },
   },
