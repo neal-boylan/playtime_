51dd7e45ddd88243c95427b1d7055097e4934ccb
neal-boylan <nealboylan@gmail.com>
2024-03-22 16:50:09 +0000

    Added mem and json tests for Users and Playlists

--- src/models/mem/playlist-mem-store.js
+++ src/models/mem/playlist-mem-store.js
@@ -15,8 +15,12 @@ export const playlistMemStore = {
   },
 
   async getPlaylistById(id) {
-    const list = playlists.find((playlist) => playlist._id === id);
-    list.tracks = await trackMemStore.getTracksByPlaylistId(list._id);
+    let list = playlists.find((playlist) => playlist._id === id);
+    if (list) {
+      list.tracks = await trackMemStore.getTracksByPlaylistId(list._id);
+    } else {
+      list = null;
+    }
     return list;
   },
 
@@ -26,7 +30,7 @@ export const playlistMemStore = {
 
   async deletePlaylistById(id) {
     const index = playlists.findIndex((playlist) => playlist._id === id);
-    playlists.splice(index, 1);
+    if (index !== -1)  playlists.splice(index, 1);
   },
 
   async deleteAllPlaylists() {
