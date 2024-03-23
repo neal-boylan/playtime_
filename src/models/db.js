13a587b595097cc28af8c82535073f34b4a052b1
neal-boylan <nealboylan@gmail.com>
2024-03-23 18:44:45 +0000

    Created mongo database, added track tests

--- src/models/db.js
+++ src/models/db.js
@@ -4,6 +4,10 @@ import { trackMemStore } from "./mem/track-mem-store.js";
 import { userJsonStore } from "./json/user-json-store.js";
 import { playlistJsonStore } from "./json/playlist-json-store.js";
 import { trackJsonStore } from "./json/track-json-store.js";
+import { connectMongo } from "./mongo/connect.js";
+import { userMongoStore } from "./mongo/user-mongo-store.js";
+import { playlistMongoStore } from "./mongo/playlist-mongo-store.js";
+import { trackMongoStore } from "./mongo/track-mongo-store.js";
 
 export const db = {
   userStore: null,
@@ -16,6 +20,12 @@ export const db = {
         this.userStore = userJsonStore;
         this.playlistStore = playlistJsonStore;
         this.trackStore = trackJsonStore;
+        break;
+      case "mongo":
+        this.userStore = userMongoStore;
+        this.playlistStore = playlistMongoStore;
+        this.trackStore = trackMongoStore;
+        connectMongo();
         break;
       default:
         this.userStore = userMemStore;
---
+++ src/models/mongo/user-mongo-store.js
@@ -0,0 +1,45 @@
+import { User } from "./user.js";
+
+export const userMongoStore = {
+  async getAllUsers() {
+    const users = await User.find().lean();
+    return users;
+  },
+
+  async getUserById(id) {
+    if (id) {
+			try{
+        const user = await User.findOne({ _id: id }).lean();
+				return user;
+			}
+			catch(err){
+			  return null;
+			}
+    }
+    return null;
+  },
+
+  async addUser(user) {
+    const newUser = new User(user);
+    const userObj = await newUser.save();
+    const u = await this.getUserById(userObj._id);
+    return u;
+  },
+
+  async getUserByEmail(email) {
+    const user = await User.findOne({ email: email }).lean();
+    return user;
+  },
+
+  async deleteUserById(id) {
+    try {
+      await User.deleteOne({ _id: id });
+    } catch (error) {
+      console.log("bad id");
+    }
+  },
+
+  async deleteAll() {
+    await User.deleteMany({});
+  }
+};
